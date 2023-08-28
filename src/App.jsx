2c609101561169b50b195d2fed7c933597178554
dageem <dagtin1@gmail.com>
2023-08-28 15:14:24 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import "./App.css";
+import { puppyList } from "./Data/data";
+import { useState } from "react";
+import "./color.css"
 
 function App() {
-
-  return (<div>
-    Hello world
-  </div>);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <div key={puppy.id}>
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+            >
+              {puppy.name}
+            </p>
+          </div>
+        );
+      })}
+      <h2>{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</h2>
+    </div>
+  );
 }
 
 export default App;
